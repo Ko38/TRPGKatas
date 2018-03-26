3b6728e3265dea519a34af9f406afeb07d399dac
Ko38 <phillipko38@outlook.com>
2018-03-26 11:13:22 +0800

    enhancement

--- LetsPlayDarts/LetsPlayDarts/Dartboard.cs
+++ LetsPlayDarts/LetsPlayDarts/Dartboard.cs
@@ -17,29 +17,8 @@ namespace LetsPlayDarts
                 {324 / 2, "{point}" },
                 {340 / 2, "D{point}" }
             };
-        private readonly Dictionary<double, string> _angleToScore = new Dictionary<double, string>()
-            {
-                {9, "6" },
-                {27, "13" },
-                {45, "4" },
-                {63, "18" },
-                {81, "1" },
-                {99, "20" },
-                {117, "5" },
-                {135, "12" },
-                {153, "9" },
-                {171, "14" },
-                {189, "11" },
-                {207, "8" },
-                {225, "16" },
-                {243, "7" },
-                {261, "19" },
-                {279, "3" },
-                {297, "17" },
-                {315, "2" },
-                {333, "15" },
-                {351, "10" }
-            };
+        
+        private readonly string[] _baseScore = {"6" ,"13","4" ,"18","1" ,"20","5" ,"12","9" ,"14","11","8" ,"16","7" ,"19","3" ,"17","2" ,"15","10"};
 
         private string GetScoreText(double distance)
         {
@@ -52,8 +31,7 @@ namespace LetsPlayDarts
             }
             return "X";
         }
-
-
+        
         public string GetScore(double x, double y)
         {
             var distance = CalculateDistanceByPythagoreanTheorem(x, y);
@@ -63,12 +41,10 @@ namespace LetsPlayDarts
 
         private string GetPoint(double angle)
         {
-            if (angle < -9) angle += 360;
-            foreach(var item in _angleToScore)
-            {
-                if(angle < item.Key) return item.Value;
-            }
-            return null;
+            angle += 9;
+            if (angle < 0) angle += 360;
+            int index = (int)angle / 18;
+            return _baseScore[index];
         }
 
         private double CalculateDistanceByPythagoreanTheorem(double x, double y)
