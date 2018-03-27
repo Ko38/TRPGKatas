39459c2c49c6d9cef44f4af62a2208a1861fa721
Ko38 <phillipko38@outlook.com>
2018-03-27 16:31:08 +0800

    new kata

---
+++ RunnersMeetings/RunnersMeetings/Kata.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunnersMeetings
+{
+    public class Kata
+    {
+        public int RunnersMeetings(int[] StartPosition, int[] speed)
+        {
+            //coding and coding..
+
+            return 0;
+        }
+    }
+
+    [TestFixture]
+    public class myjinxin
+    {
+
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new Kata();
+            Assert.AreEqual(3, kata.RunnersMeetings(new int[] { 1, 4, 2 }, new int[] { 27, 18, 24 }));
+            Assert.AreEqual(2, kata.RunnersMeetings(new int[] { 1, 4, 2 }, new int[] { 5, 6, 2 }));
+            Assert.AreEqual(-1, kata.RunnersMeetings(new int[] { 1, 2, 3 }, new int[] { 1, 1, 1 }));
+            Assert.AreEqual(2, kata.RunnersMeetings(new int[] { 1, 1000 }, new int[] { 23, 22 }));
+        }
+
+    }
+}
